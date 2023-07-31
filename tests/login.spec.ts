b57e81c5b312ab2203efa1afbfe7e5f720d392b0
meureuet <gmdgjfr@gmail.com>
2023-08-01 00:19:48 +0900

    로그인 테스트

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,20 +1,88 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
 });
 
-test('login success', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
+test('success', async ({ page }) => {
+  const username: string = 'standard_user'
+  const password: string = 'secret_sauce'
 
-  const header = page.locator('.header_container');
-  await expect(header).toBeEnabled();
-
-  const inventory = page.locator('.inventory_container');
-  await expect(inventory).toBeEnabled();
+  await login(page, username, password)
+  
+  await expect(page).toHaveURL(/inventory.html/);
 });
+
+test('not match username', async ({ page }) => {
+  const username: string = 'standard_use'
+  const password: string = 'secret_sauce'
+
+  await login(page, username, password)
+  
+  const errorLocator = page.locator('.error-message-container h3')
+
+  await expect(errorLocator).toHaveText('Epic sadface: Username and password do not match any user in this service')
+});
+
+test('not match password', async ({ page }) => {
+  const username: string = 'standard_user'
+  const password: string = 'secret_sauc'
+
+  await login(page, username, password)
+  
+  const errorLocator = page.locator('.error-message-container h3')
+
+  await expect(errorLocator).toHaveText('Epic sadface: Username and password do not match any user in this service')
+});
+
+test('login locked user', async ({ page }) => {
+  const username: string = 'locked_out_user'
+  const password: string = 'secret_sauce'
+
+  await login(page, username, password)
+  
+  const errorLocator = page.locator('.error-message-container h3')
+
+  await expect(errorLocator).toHaveText('Epic sadface: Sorry, this user has been locked out.')
+});
+
+test('not input username', async ({ page }) => {
+  const username: string = ''
+  const password: string = 'secret_sauce'
+
+  await login(page, username, password)
+  
+  const errorLocator = page.locator('.error-message-container h3')
+
+  await expect(errorLocator).toHaveText('Epic sadface: Username is required')
+});
+
+test('not input password', async ({ page }) => {
+  const username: string = 'standard_user'
+  const password: string = ''
+
+  await login(page, username, password)
+  
+  const errorLocator = page.locator('.error-message-container h3')
+
+  await expect(errorLocator).toHaveText('Epic sadface: Password is required')
+});
+
+test('not input username and password', async ({ page }) => {
+  const username: string = ''
+  const password: string = ''
+
+  await login(page, username, password)
+  
+  const errorLocator = page.locator('.error-message-container h3')
+
+  await expect(errorLocator).toHaveText('Epic sadface: Username is required')
+});
+
+async function login(page: Page, username: string, password: string) {
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill(username);
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill(password);
+  await page.locator('[data-test="login-button"]').click();
+}
